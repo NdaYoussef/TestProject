1afc21caec4e8d88bd3c6da2274ed628c3ef4516
Nda Youssef <140206711+NdaYoussef@users.noreply.github.com>
2024-11-01 18:39:29 +0200

    Add files via upload

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestProject.Models
+{
+    public class ApplicationContext : DbContext 
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+
+        internal async Task<IActionResult> FindAsync(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestProject.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+       
+    }
+}
