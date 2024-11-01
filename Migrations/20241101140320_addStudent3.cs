ab94c7b11bf69dcd2ee0f3f1ce263c9d61499b30
Nda Youssef <140206711+NdaYoussef@users.noreply.github.com>
2024-11-01 18:38:11 +0200

    Add files via upload

---
+++ Migrations/20241101140320_addStudent3.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class addStudent3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Students");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Students");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Students",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
